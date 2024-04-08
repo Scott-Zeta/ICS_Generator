e121eb26158bebe76ba9ae5cf71b08991cfcee3d
Scott Zeta <scott-zeta@outlook.com>
2024-04-08 18:10:12 +0930

    mounting app on django project

--- ics_gen_backend/ics_gen_backend/settings.py
+++ ics_gen_backend/ics_gen_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'generator_core.apps.GeneratorCoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
