615e5e1cd597c6f9753d52236f909547fe10e347
Scott Zeta <scott-zeta@outlook.com>
2024-04-09 14:09:54 +0930

    implement the server time

--- ics_gen_backend/generator_core/views.py
+++ ics_gen_backend/generator_core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.utils import timezone
+import datetime
 
 # Create your views here.
 def index(request):
     template = loader.get_template('generator_core/index.html')
-    context ={"title": "ICS Generator"}
+    server_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+    context ={"title": "ICS Generator", "server_time": server_time}
     return HttpResponse(HttpResponse(template.render(context, request)))
 
 def upload(request):
