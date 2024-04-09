13489236ea4aac04fae8028b39661d92b56d3753
Scott Zeta <scott-zeta@outlook.com>
2024-04-09 12:52:22 +0930

    add view template for entry point

--- ics_gen_backend/generator_core/views.py
+++ ics_gen_backend/generator_core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Generator Core Index.")
+    template = loader.get_template('generator_core/index.html')
+    context ={"title": "ICS Generator"}
+    return HttpResponse(HttpResponse(template.render(context, request)))
 
 def upload(request):
-    return HttpResponse("Upload entry point.")
+    template = loader.get_template('generator_core/upload.html')
+    context ={"title": "Upload Entry Point"}
+    return HttpResponse(HttpResponse(template.render(context, request)))
