7ea6b00ce08ef659460b86d1a3eb01619dbb8357
Scott Zeta <scott-zeta@outlook.com>
2024-04-09 13:24:44 +0930

    add two entry point ticket record models

--- ics_gen_backend/generator_core/models.py
+++ ics_gen_backend/generator_core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Ticket Record for Upload entry hit point
+class UploadRecord(models.Model):
+    acess_time = models.DateTimeField(auto_now_add=True)
+    
+# Ticket Record for Download entry hit point
+class DownloadRecord(models.Model):
+    acess_time = models.DateTimeField(auto_now_add=True)
