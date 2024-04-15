10c168972aaa39a2b84e7e16f0525cb1abd5500f
Scott Zeta <scott-zeta@outlook.com>
2024-04-15 12:17:34 +0930

    Implement generic view on Index page (chang from function-based to class-based)

--- ics_gen_backend/generator_core/urls.py
+++ ics_gen_backend/generator_core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from .views import IndexView
 from . import views
 
+app_name = "generator_core"
 urlpatterns = [
     # ex: /generator_core/
-    path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
     # ex: /generator_core/upload/
     path("upload/", views.upload, name="upload"),
     # ex: /generator_core/download/
--- ics_gen_backend/generator_core/views.py
+++ ics_gen_backend/generator_core/views.py
@@ -1,19 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.http import HttpResponse
-from django.template import loader
 from django.utils import timezone
 from .models import UploadRecord, DownloadRecord
 
-# Create your views here.
-def index(request):
-    template = loader.get_template('generator_core/index.html')
-    server_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
-    recent_upload_record = UploadRecord.objects.order_by('-access_time')[:5]
-    recent_download_record = DownloadRecord.objects.order_by('-access_time')[:5]
-    context ={"title": "ICS Generator", "server_time": server_time, 
-              "recent_upload_record": recent_upload_record,
-              "recent_download_record": recent_download_record}
-    return HttpResponse(template.render(context, request))
+# Previous View
+# def index(request):
+#     template = loader.get_template('generator_core/index.html')
+#     server_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+#     recent_upload_record = UploadRecord.objects.order_by('-access_time')[:5]
+#     recent_download_record = DownloadRecord.objects.order_by('-access_time')[:5]
+#     context ={"title": "ICS Generator", "server_time": server_time, 
+#               "recent_upload_record": recent_upload_record,
+#               "recent_download_record": recent_download_record}
+#     return HttpResponse(template.render(context, request))
+
+# Generic View
+class IndexView(TemplateView):
+    template_name = "generator_core/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        server_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+        recent_upload_record = UploadRecord.objects.order_by('-access_time')[:5]
+        recent_download_record = DownloadRecord.objects.order_by('-access_time')[:5]
+        context["title"] = "ICS Generator"
+        context["server_time"] = server_time
+        context["recent_upload_record"] = recent_upload_record
+        context["recent_download_record"] = recent_download_record
+        return context
 
 def upload(request):
     context ={}
