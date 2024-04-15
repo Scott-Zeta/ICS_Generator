38e14b9c54b6418b917b41a6bdaefa212f7d5a74
Scott Zeta <scott-zeta@outlook.com>
2024-04-15 15:48:06 +0930

    Image input validation

--- ics_gen_backend/generator_core/serializers.py
+++ ics_gen_backend/generator_core/serializers.py
@@ -1,5 +1,27 @@
 from rest_framework import serializers
+from PIL import Image
 
 class PostSerializer(serializers.Serializer):
     text = serializers.CharField(required=False, allow_blank=True, max_length=500, trim_whitespace=True)
     
+    image = serializers.ImageField(required=False, allow_empty_file=True, max_length=50, use_url=False)
+    
+    def validate(self,data):
+        if not data.get('text') and not data.get('image'):
+            raise serializers.ValidationError("Both text and image can not be empty")
+        return data
+    
+    def validate_image(self, value):
+        if value:
+            if value.size > 5 * 1024*1024:
+                raise serializers.ValidationError("File can not larger than 5MB")
+            
+            try:
+                img = Image.open(value)
+                if not value.image.format.lower() in ['jpg','jpeg','png','webp','svg','gif']:
+                    raise serializers.ValidationError("File format not supported")
+            except Exception:
+                raise serializers.ValidationError("File is not a valid image")
+            
+            img.close()
+        return value
