58dc8865631271a9d45f57df760d5ea2755a0f5b
Scott Zeta <scott-zeta@outlook.com>
2024-04-15 13:30:25 +0930

    modified template and index time stamp

--- ics_gen_backend/generator_core/models.py
+++ ics_gen_backend/generator_core/models.py
@@ -6,6 +6,12 @@ from django.db import models
 class UploadRecord(models.Model):
     access_time = models.DateTimeField(auto_now_add=True)
     
+    def formatted_time(self):
+        return self.access_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+    
 # Ticket Record for Download entry hit point
 class DownloadRecord(models.Model):
     access_time = models.DateTimeField(auto_now_add=True)
+    
+    def formatted_time(self):
+        return self.access_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
