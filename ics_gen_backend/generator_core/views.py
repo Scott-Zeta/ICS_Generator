ae617eb67b974d44c7c181e6e9abc3c463fd50c6
Scott Zeta <scott-zeta@outlook.com>
2024-04-15 13:37:01 +0930

    delete comments

--- ics_gen_backend/generator_core/views.py
+++ ics_gen_backend/generator_core/views.py
@@ -4,16 +4,6 @@ from django.utils import timezone
 from .models import UploadRecord, DownloadRecord
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-# Previous View
-# def index(request):
-#     template = loader.get_template('generator_core/index.html')
-#     server_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
-#     recent_upload_record = UploadRecord.objects.order_by('-access_time')[:5]
-#     recent_download_record = DownloadRecord.objects.order_by('-access_time')[:5]
-#     context ={"title": "ICS Generator", "server_time": server_time, 
-#               "recent_upload_record": recent_upload_record,
-#               "recent_download_record": recent_download_record}
-#     return HttpResponse(template.render(context, request))
 
 # Generic View
 class IndexView(TemplateView):
