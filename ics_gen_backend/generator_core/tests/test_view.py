77bcde20b43192873cd3f9df87aef34a7e98b697
Scott Zeta <scott-zeta@outlook.com>
2024-04-22 13:04:52 +0930

    add invalid test case

--- ics_gen_backend/generator_core/tests/test_view.py
+++ ics_gen_backend/generator_core/tests/test_view.py
@@ -31,6 +31,30 @@ class UploadViewTests(APITestCase):
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
             self.assertEqual(response.data, {"Message": "Valid Input"})
             
+    def test_invalid_input(self):
+        valid_text = "a" * 500
+        invalid_text = "a" * 501
+        test_cases = [
+            {'text': invalid_text, 'file': 'valid_image.png'},
+            {'text': valid_text, 'file': 'large_image.png'},
+            {'text': valid_text, 'file': 'unsupported_format.svg'},
+            {'text': valid_text, 'file': 'not_image.txt'},
+            {}
+        ]
+        
+        for case in test_cases:
+            data = {}
+            if 'text' in case:
+                data['text'] = case['text']
+            if 'file' in case:
+                # Have to open file multiple time because of the file pointer in file stream
+                with open(f'generator_core/tests/sample_images/{case["file"]}', 'rb') as file:
+                    uploaded_file = SimpleUploadedFile(case["file"], file.read(), content_type='image/png')
+                    data['image'] = uploaded_file
+
+            response = self.client.post(self.url, data, format='multipart')
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            
     def test_invalid_request_format(self):
         # Test Invalid Request Format
         data = {'text': 'a' * 500}
