167e9c668a4d60a198fc7ded2ccf50c91dd7b5ed
Scott Zeta <scott-zeta@outlook.com>
2024-04-24 17:45:41 +0930

    Pytesseract base case

--- ics_gen_backend/generator_core/utils/ocr_util.py
+++ ics_gen_backend/generator_core/utils/ocr_util.py
@@ -1,2 +1,8 @@
 from PIL import Image
 import pytesseract
+
+with Image.open('../tests/sample_images/image_67178753.JPG') as img:
+    dpi = img.info.get('dpi')
+    print(dpi)
+    print(pytesseract.image_to_osd(img))
+    print(pytesseract.image_to_string(img))
