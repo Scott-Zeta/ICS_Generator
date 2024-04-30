14def05e9843da3a70dc50e35c8cb146c5e015fb
Scott Zeta <scott-zeta@outlook.com>
2024-04-30 21:13:45 +0930

    upgrade to api version 4, increase accuracy and cut off result fetching

--- ics_gen_backend/generator_core/utils/ocr_azure_util.py
+++ ics_gen_backend/generator_core/utils/ocr_azure_util.py
@@ -1,22 +1,16 @@
 import os
 from dotenv import load_dotenv
 import requests
-import time
-import json
-from PIL import Image
-from io import BytesIO
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
 
 load_dotenv()
 
 # Set the subscription key and endpoint for the OCR service
 endpoint = os.getenv('AZURE_OCR_ENDPOINT')
 subscription_key = os.getenv('AZURE_OCR_KEY')
-text_recognition_url = endpoint + "/vision/v3.2/read/analyze"
+text_recognition_url = endpoint + "/computervision/imageanalysis:analyze?api-version=2024-02-01&features=read&model-version=latest"
 
 # Set image_url to the URL of an image that you want to recognize.
-image_path = "../tests/sample_images/chinese_tra.jpg"
+image_path = "../tests/sample_images/image_67161601.jpeg"
 headers = {'Ocp-Apim-Subscription-Key': subscription_key,
            'Content-Type': 'application/octet-stream'}
 image_data = open(image_path, 'rb').read()
@@ -24,42 +18,5 @@ response = requests.post(
     text_recognition_url, headers=headers, data=image_data)
 response.raise_for_status()
 
-# Extracting text requires two API calls: One call to submit the
-# image for processing, the other to retrieve the text found in the image.
-
-# Holds the URI used to retrieve the recognized text.
-operation_url = response.headers["Operation-Location"]
-
-# The recognized text isn't immediately available, so poll to wait for completion.
-analysis = {}
-poll = True
-while (poll):
-    response_final = requests.get(
-        response.headers["Operation-Location"], headers=headers)
-    analysis = response_final.json()
-    
-    print(json.dumps(analysis, indent=4))
-
-    time.sleep(1)
-    if ("analyzeResult" in analysis):
-        poll = False
-    if ("status" in analysis and analysis['status'] == 'failed'):
-        poll = False
-
-polygons = []
-if ("analyzeResult" in analysis):
-    # Extract the recognized text, with bounding boxes.
-    polygons = [(line["boundingBox"], line["text"])
-                for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
-
-# Display the image and overlay it with the extracted text.
-image = Image.open(BytesIO(image_data))
-ax = plt.imshow(image)
-for polygon in polygons:
-    vertices = [(polygon[0][i], polygon[0][i+1])
-                for i in range(0, len(polygon[0]), 2)]
-    text = polygon[1]
-    patch = Polygon(vertices, closed=True, fill=False, linewidth=2, color='y')
-    ax.axes.add_patch(patch)
-    plt.text(vertices[0][0], vertices[0][1], text, fontsize=20, va="top")
-plt.show()
+analysis = response.json()
+print(analysis)
