b840382145a8844c03fc265b564c4980efdd5b0a
Scott Zeta <scott-zeta@outlook.com>
2024-05-02 17:42:13 +0930

    Integrate azure OCR

--- ics_gen_backend/generator_core/utils/ocr/ocr_azure_util.py
+++ ics_gen_backend/generator_core/utils/ocr/ocr_azure_util.py
@@ -2,35 +2,52 @@ import os
 from dotenv import load_dotenv
 import requests
 
-load_dotenv()
+class OCR_Azure_Exception(Exception):
+    """Exception for errors encountered with Azure OCR service."""
+    pass
 
-# Set the subscription key and endpoint for the OCR service
-endpoint = os.getenv('AZURE_OCR_ENDPOINT')
-subscription_key = os.getenv('AZURE_OCR_KEY')
-api_config = os.getenv('OCR_API_CONFIG')
-text_recognition_url = endpoint + api_config
-
-# Set image_url to the URL of an image that you want to recognize.
-image_path = "../../tests/sample_images/handwriting_printed_mixed_photo.jpeg"
-headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-           'Content-Type': 'application/octet-stream'}
-image_data = open(image_path, 'rb').read()
-response = requests.post(
-    text_recognition_url, headers=headers, data=image_data)
-response.raise_for_status()
-
-analysis = response.json()
-
-if 'readResult' in analysis:
-    # Extract the text from the OCR result
-    read_result_blocks = analysis['readResult']['blocks']
-    plaintext = []
-    for block in read_result_blocks:
-        if 'lines' in block:
-            for line in block['lines']:
-                plaintext.append(line['text'])
-    print(" ".join(plaintext))
-else:
-    print("No readResult")
+def ocr_azure(validated_image):
+    # validated_image should be an image in binary data that has passed validation
     
+    # Set up Azure OCR API and request headers
+    load_dotenv()
+    endpoint = os.getenv('AZURE_OCR_ENDPOINT')
+    subscription_key = os.getenv('AZURE_OCR_KEY')
+    api_config = os.getenv('OCR_API_CONFIG')
     
+    if not all([endpoint, subscription_key, api_config]):
+        raise OCR_Azure_Exception("Missing required Azure OCR configuration.")
+
+    text_recognition_url = endpoint + api_config
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+                'Content-Type': 'application/octet-stream'}
+    # image_data = open(validated_image, 'rb').read()
+
+    # Send the request, parse the response and extract the text if successful
+    try:
+        response = requests.post(
+            text_recognition_url, headers = headers, data = validated_image)
+        
+        # If not success code, goes into except block
+        response.raise_for_status()
+
+        analysis = response.json()
+
+        if 'readResult' in analysis:
+            # Extract the text from the OCR result
+            read_result_blocks = analysis['readResult']['blocks']
+            plaintext = []
+            for block in read_result_blocks:
+                if 'lines' in block:
+                    for line in block['lines']:
+                        plaintext.append(line['text'])
+            return " ".join(plaintext)
+        else:
+            return ""
+    except requests.exceptions.HTTPError:
+        error_message = response.json().get('error', {}).get('message', 'Unknown Azure service error')
+        raise OCR_Azure_Exception(f"Error: {error_message}")
+    except Exception as e:
+        raise OCR_Azure_Exception(f"Internal Error: {str(e)}")
+
+            
