97aa89eb83ca3d0f884abd6d28b8c277d5d95f43
Scott Zeta <scott-zeta@outlook.com>
2024-05-19 17:20:10 +0930

    Promt version 1

--- ics_gen_backend/generator_core/utils/nlp/nlp_openai_util.py
+++ ics_gen_backend/generator_core/utils/nlp/nlp_openai_util.py
@@ -7,9 +7,13 @@ client = OpenAI(api_key = os.getenv('OPENAI_NLP_KEY'))
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
+  response_format={ "type": "json_object" },
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are an assitant help convert the event information included in nature language to JSON output."},
+    {"role": "system", "content": "Your output format always follow the following guidelines: {\"events\": [{\"name\":'The name of the event', \"begin\": 'The begin time of the event, always write in ISO time format, if timezone not been specific mentioned, leave it as local time format', \"end\": 'The end time of the event, always write in ISO time format, if timezone not been specific mentioned, leave it as local time format', \"location\": 'The location of the event, put the url if it is an online event', \"description\": The description of the event, \"url\":'the url that relative to the event', \"organizer\": 'Write the organiser as an ics file required format', \"category\": 'The category of the event', \"RRULE\": 'The Recurring event rule, it shall be write in an ics file required format'}, ...more events in the array]}"},
+    {"role": "system", "content": "If the date format in nature language provide by user is not clear, determine based on the language or location context. If still can not be determined, use british english date format."},
+    {"role": "system", "content": "You shall not provide any information not relative to an event. Anything uncertain or not provided by user, you shall leave it as an empty string in your output."},
+    {"role": "user", "content": ""}
   ]
 )
 
